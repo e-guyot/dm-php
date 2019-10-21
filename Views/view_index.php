1fb0455b6c151c64959a8e351ccaa986dc6bbb87
eguyot <guyot.estellee@gmail.com>
2019-10-21 17:08:32 +0200

    update title

--- Views/view_index.php
+++ Views/view_index.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Forum</title>
+  <title>Squeegee's Forum</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-  <header>
-    <h1>Hello on this forum</h1>
-  </header>
   <?php include_once(__DIR__.'/view_navbar.php'); ?>
   <main>
-    
+    <div class="container">
+      <h2 class="text-center">Welcome to the Squeegee's forum !</h2>
+    </div>
   </main>
   <?php include_once(__DIR__.'/view_footer.php'); ?>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
--- Views/view_newPost.php
+++ Views/view_newPost.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Forum</title>
+  <title>Squeegee's Forum</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-  <header>
-    <h1>Hello on this forum</h1>
-  </header>
   <?php include_once(__DIR__.'/view_navbar.php'); ?>
   <main>
     <div class="container">
